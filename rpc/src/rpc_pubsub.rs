3270fcdf2ab16fbf53b499d4de5244683e7ba8d6
yusufyian <yussufier@gmail.com>
2024-06-17 08:02:47 +0000

    Fix typo in JSON-RPC error message

--- rpc/src/rpc_pubsub.rs
+++ rpc/src/rpc_pubsub.rs
@@ -846,7 +846,7 @@ mod tests {
         // Test bad parameter
         let req = r#"{"jsonrpc":"2.0","id":1,"method":"signatureUnsubscribe","params":[1]}"#;
         let res = io.handle_request_sync(req);
-        let expected = r#"{"jsonrpc":"2.0","error":{"code":-32602,"message":"Invalid subscription id."},"id":1}"#;
+        let expected = r#"{"jsonrpc":"2.0","error":{"code":-32602,"message":"Invalid subscription id"},"id":1}"#;
         let expected: Response = serde_json::from_str(expected).unwrap();
 
         let result: Response = serde_json::from_str(&res.unwrap()).unwrap();
@@ -1119,7 +1119,7 @@ mod tests {
         // Test bad parameter
         let req = r#"{"jsonrpc":"2.0","id":1,"method":"accountUnsubscribe","params":[1]}"#;
         let res = io.handle_request_sync(req);
-        let expected = r#"{"jsonrpc":"2.0","error":{"code":-32602,"message":"Invalid subscription id."},"id":1}"#;
+        let expected = r#"{"jsonrpc":"2.0","error":{"code":-32602,"message":"Invalid subscription id"},"id":1}"#;
         let expected: Response = serde_json::from_str(expected).unwrap();
 
         let result: Response = serde_json::from_str(&res.unwrap()).unwrap();
