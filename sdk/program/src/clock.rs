946dc9894e60f3b622816024402c147602c194f6
yusufyian <yussufier@gmail.com>
2024-07-17 06:53:11 +0000

    chore: Update DEFAULT_TICKS_PER_SECOND and MS_PER_TICK constants

--- sdk/program/src/clock.rs
+++ sdk/program/src/clock.rs
@@ -25,10 +25,10 @@ use solana_sdk_macro::CloneZeroed;
 /// The default tick rate that the cluster attempts to achieve (160 per second).
 ///
 /// Note that the actual tick rate at any given time should be expected to drift.
-pub const DEFAULT_TICKS_PER_SECOND: u64 = 320;
+pub const DEFAULT_TICKS_PER_SECOND: u64 = 960;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(MS_PER_TICK, 3);
+static_assertions::const_assert_eq!(MS_PER_TICK, 1);
 
 /// The number of milliseconds per tick (6).
 pub const MS_PER_TICK: u64 = 1000 / DEFAULT_TICKS_PER_SECOND;
@@ -39,7 +39,7 @@ pub const SLOT_MS: u64 = DEFAULT_MS_PER_SLOT;
 
 // At 160 ticks/s, 64 ticks per slot implies that leader rotation and voting will happen
 // every 400 ms. A fast voting cadence ensures faster finality and convergence
-pub const DEFAULT_TICKS_PER_SLOT: u64 = 64;
+pub const DEFAULT_TICKS_PER_SLOT: u64 = 16;
 
 // GCP n1-standard hardware and also a xeon e5-2520 v4 are about this rate of hashes/s
 pub const DEFAULT_HASHES_PER_SECOND: u64 = 2_000_000;
