b4f9852e30da2af37814d9a158fdca3727c72ac3
ZeneDeLuca <zenedeluca@gmail.com>
2024-09-09 11:12:59 +0000

    chore: Add state module for migrated accounts
    Added a new module called `state` in the `sonic_account_migrater` directory. This module contains the definition of the `MigratedAccountsState` enum and the `MigratedAccount` struct. These types represent the different states of the program accounts and the details of each migrated account.
    
    Refactor the `instruction` module
    
    Refactored the `instruction` module in the `sonic_account_migrater` directory. Removed the `migrate_remote_accounts` and `deactivate_remote_accounts` functions and replaced them with new variants of the `ProgramInstruction` enum. The new variants, `MigrateRemoteAccounts` and `DeactivateRemoteAccounts`, now include a vector of addresses as a parameter.
    
    Update `mod.rs` file
    
    Added a new module called `migrated_accounts` in the `sonic_account_migrater` directory. This module is used to declare the ID for the `MigratedAccounts` program account.

--- sdk/program/src/sonic_account_migrater/mod.rs
+++ sdk/program/src/sonic_account_migrater/mod.rs
@@ -3,10 +3,13 @@
 //! [np]: 
 
 pub mod instruction;
+pub mod state;
 
 
 pub mod program {
     crate::declare_id!("SonicAccountMigrater11111111111111111111111");
 }
 
-
+pub mod migrated_accounts {
+    crate::declare_id!("SonicMigratedAccounts1111111111111111111112");
+}
