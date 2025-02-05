c6c46807e45cd6891ee3e2c4791344ef31be8c46
codebender828 <codebender828@gmail.com>
2025-02-06 00:37:46 +0300

    fix: add mainnet-alphs genesis hash

--- sdk/src/genesis_config.rs
+++ sdk/src/genesis_config.rs
@@ -56,7 +56,7 @@ impl ClusterType {
     pub fn get_genesis_hash(&self) -> Option<Hash> {
         match self {
             Self::MainnetBeta => {
-                Some(Hash::from_str("5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d").unwrap())
+                Some(Hash::from_str("9qoRTAHGWBZHYzMJGkt62wBbFRASj6H7CvoNsNyRw2h4").unwrap())
             }
             Self::Testnet => {
                 Some(Hash::from_str("4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY").unwrap())
