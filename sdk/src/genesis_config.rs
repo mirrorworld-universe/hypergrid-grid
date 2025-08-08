2c6d3a6174e4c7ac3715b183675ebd0e64d0713c
kquirapas <kmquirapas@gmail.com>
2025-08-08 12:24:46 +0000

    fix: Change Devnet genesis hash

--- sdk/src/genesis_config.rs
+++ sdk/src/genesis_config.rs
@@ -62,7 +62,7 @@ impl ClusterType {
                 Some(Hash::from_str("4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY").unwrap())
             }
             Self::Devnet => {
-                Some(Hash::from_str("EtWTRABZaYq6iMfeYKouRu166VU2xqa1wcaWoxPkrZBG").unwrap())
+                Some(Hash::from_str("AkeT79MNAcsWeuECsTW7vnz6K3w5zqUWkgHYTZSZeJWT").unwrap())
             }
             Self::Development => None,
         }
