0ad5ad5e2b9e366387bbd3dabbe4ce85d0408c06
i1i1 <vanyarybin1@live.ru>
2025-09-24 16:26:37 +0300

    Make `hypergrid/src/cosmos.rs` more idiomatic

--- hypergrid/src/cosmos.rs
+++ hypergrid/src/cosmos.rs
@@ -1,29 +1,19 @@
-use {log::*, std::process::Command};
+use {log::*, solana_sdk::pubkey::Pubkey, std::process::Command};
 
 const COSMOS_CHAIN_ID: &str = "hypergridssn";
 const COSMOS_HOME: &str = ".hypergrid-ssn";
 const COSMOS_APP: &str = "bin/hypergrid-ssnd";
 const COSMOS_SIGNER: &str = "my_key";
 
-pub fn run_load_solana_account(pub_key: &str, version: &str, source: &str, update: bool) {
+pub fn run_load_solana_account(pub_key: &Pubkey, version: &str, source: &Pubkey, update: bool) {
     let home_path = dirs_next::home_dir().expect("home directory");
+    let (pub_key, source) = (pub_key.to_string(), source.to_string());
 
-    let cosmos_home_path = {
-        let mut _path = home_path.clone();
-        _path.extend([COSMOS_HOME]);
-        _path.to_str().unwrap().to_string()
-    };
-
-    let cosmos_app_path = {
-        let mut _path = home_path.clone();
-        _path.extend([COSMOS_HOME, COSMOS_APP]);
-        _path.to_str().unwrap().to_string()
-    };
+    let cosmos_app_path = home_path.join(COSMOS_HOME).join(COSMOS_APP);
 
     let app_path = std::path::Path::new(&cosmos_app_path);
     if !app_path.exists() {
-        warn!("{} does not exist.", cosmos_app_path);
-        // println!("{} does not exist.", cosmos_app_path);
+        warn!("{cosmos_app_path:?} does not exist.");
         return;
     }
 
@@ -32,11 +22,14 @@ pub fn run_load_solana_account(pub_key: &str, version: &str, source: &str, updat
     cmd.arg("tx")
         .arg("hypergridssn")
         .args(&if update {
-            vec!["update-solana-account", pub_key, version]
+            vec!["update-solana-account", &pub_key, version]
         } else {
-            vec!["create-solana-account", pub_key, version, source]
+            vec!["create-solana-account", &pub_key, version, &source]
         })
-        .args(["--home", &cosmos_home_path])
+        .args([
+            AsRef::<std::ffi::OsStr>::as_ref("--home"),
+            home_path.join(COSMOS_HOME).as_ref(),
+        ])
         .args(["--from", COSMOS_SIGNER])
         .args(["--chain-id", COSMOS_CHAIN_ID])
         .args(["--gas", "50000000"])
