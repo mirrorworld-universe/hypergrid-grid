c5aded625ac568ae3233078c1d6e2bc0dbedc953
i1i1 <vanyarybin1@live.ru>
2025-11-24 13:50:02 +0300

    Decrease block time

--- sdk/clock/src/lib.rs
+++ sdk/clock/src/lib.rs
@@ -25,13 +25,16 @@
 use serde_derive::{Deserialize, Serialize};
 use solana_sdk_macro::CloneZeroed;
 
-/// The default tick rate that the cluster attempts to achieve (160 per second).
+/// SONIC: increase time by this factor
+pub const SONIC_TIME_FACTOR: u64 = 2;
+
+/// The default tick rate that the cluster attempts to achieve (320 per second).
 ///
 /// Note that the actual tick rate at any given time should be expected to drift.
-pub const DEFAULT_TICKS_PER_SECOND: u64 = 160;
+pub const DEFAULT_TICKS_PER_SECOND: u64 = 160 * SONIC_TIME_FACTOR; // SONIC:
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(MS_PER_TICK, 6);
+static_assertions::const_assert_eq!(MS_PER_TICK, 6 / SONIC_TIME_FACTOR);
 
 /// The number of milliseconds per tick (6).
 pub const MS_PER_TICK: u64 = 1000 / DEFAULT_TICKS_PER_SECOND;
@@ -57,27 +60,27 @@ pub const UPDATED_HASHES_PER_SECOND_5: u64 = 9_200_000;
 pub const UPDATED_HASHES_PER_SECOND_6: u64 = 10_000_000;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(DEFAULT_HASHES_PER_TICK, 12_500);
+static_assertions::const_assert_eq!(DEFAULT_HASHES_PER_TICK, 12_500 / SONIC_TIME_FACTOR);
 pub const DEFAULT_HASHES_PER_TICK: u64 = DEFAULT_HASHES_PER_SECOND / DEFAULT_TICKS_PER_SECOND;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK2, 17_500);
+static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK2, 17_500 / SONIC_TIME_FACTOR);
 pub const UPDATED_HASHES_PER_TICK2: u64 = UPDATED_HASHES_PER_SECOND_2 / DEFAULT_TICKS_PER_SECOND;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK3, 27_500);
+static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK3, 27_500 / SONIC_TIME_FACTOR);
 pub const UPDATED_HASHES_PER_TICK3: u64 = UPDATED_HASHES_PER_SECOND_3 / DEFAULT_TICKS_PER_SECOND;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK4, 47_500);
+static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK4, 47_500 / SONIC_TIME_FACTOR);
 pub const UPDATED_HASHES_PER_TICK4: u64 = UPDATED_HASHES_PER_SECOND_4 / DEFAULT_TICKS_PER_SECOND;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK5, 57_500);
+static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK5, 57_500 / SONIC_TIME_FACTOR);
 pub const UPDATED_HASHES_PER_TICK5: u64 = UPDATED_HASHES_PER_SECOND_5 / DEFAULT_TICKS_PER_SECOND;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK6, 62_500);
+static_assertions::const_assert_eq!(UPDATED_HASHES_PER_TICK6, 62_500 / SONIC_TIME_FACTOR);
 pub const UPDATED_HASHES_PER_TICK6: u64 = UPDATED_HASHES_PER_SECOND_6 / DEFAULT_TICKS_PER_SECOND;
 
 // 1 Dev Epoch = 400 ms * 8192 ~= 55 minutes
@@ -88,11 +91,11 @@ static_assertions::const_assert_eq!(SECONDS_PER_DAY, 86_400);
 pub const SECONDS_PER_DAY: u64 = 24 * 60 * 60;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(TICKS_PER_DAY, 13_824_000);
+static_assertions::const_assert_eq!(TICKS_PER_DAY, 13_824_000 * SONIC_TIME_FACTOR);
 pub const TICKS_PER_DAY: u64 = DEFAULT_TICKS_PER_SECOND * SECONDS_PER_DAY;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(DEFAULT_SLOTS_PER_EPOCH, 432_000);
+static_assertions::const_assert_eq!(DEFAULT_SLOTS_PER_EPOCH, 432_000 * SONIC_TIME_FACTOR);
 
 /// The number of slots per epoch after initial network warmup.
 ///
@@ -103,7 +106,7 @@ pub const DEFAULT_SLOTS_PER_EPOCH: u64 = 2 * TICKS_PER_DAY / DEFAULT_TICKS_PER_S
 pub const NUM_CONSECUTIVE_LEADER_SLOTS: u64 = 4;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(DEFAULT_MS_PER_SLOT, 400);
+static_assertions::const_assert_eq!(DEFAULT_MS_PER_SLOT, 400 / SONIC_TIME_FACTOR);
 /// The expected duration of a slot (400 milliseconds).
 pub const DEFAULT_MS_PER_SLOT: u64 = 1_000 * DEFAULT_TICKS_PER_SLOT / DEFAULT_TICKS_PER_SECOND;
 pub const DEFAULT_S_PER_SLOT: f64 = DEFAULT_TICKS_PER_SLOT as f64 / DEFAULT_TICKS_PER_SECOND as f64;
@@ -119,13 +122,13 @@ pub const DEFAULT_S_PER_SLOT: f64 = DEFAULT_TICKS_PER_SLOT as f64 / DEFAULT_TICK
 pub const MAX_HASH_AGE_IN_SECONDS: usize = 120;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(MAX_RECENT_BLOCKHASHES, 300);
+static_assertions::const_assert_eq!(MAX_RECENT_BLOCKHASHES, 300 * (SONIC_TIME_FACTOR as usize));
 // Number of maximum recent blockhashes (one blockhash per non-skipped slot)
 pub const MAX_RECENT_BLOCKHASHES: usize =
     MAX_HASH_AGE_IN_SECONDS * DEFAULT_TICKS_PER_SECOND as usize / DEFAULT_TICKS_PER_SLOT as usize;
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(MAX_PROCESSING_AGE, 150);
+static_assertions::const_assert_eq!(MAX_PROCESSING_AGE, 150 * (SONIC_TIME_FACTOR as usize));
 // The maximum age of a blockhash that will be accepted by the leader
 pub const MAX_PROCESSING_AGE: usize = MAX_RECENT_BLOCKHASHES / 2;
 
